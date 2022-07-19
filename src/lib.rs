ed2415345398cf083338fd5fbbb79d0f56045a66
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2022-07-19 17:38:30 -0300

    feat: add delay and kill signal flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::env;
+use std::{env, thread};
 use std::io::{self, Read};
-use std::process::Command;
+use std::process::{Command, Child};
 use std::time::Duration;
 use std::sync::mpsc::{Receiver, RecvTimeoutError};
 
@@ -8,6 +8,9 @@ use thiserror::Error;
 use anyhow::{Context, Result};
 use notify::{DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
 use structopt::StructOpt;
+
+use nix::unistd::Pid;
+use nix::sys::signal::{self, Signal};
 
 #[derive(Debug, Error)]
 pub enum IoWatchError {
@@ -39,8 +42,21 @@ pub struct IoWatch {
     /// The amount of seconds to wait until the command is executed if no events have been fired
     #[structopt(short = "t")]
     timeout: Option<u64>,
+    /// The time delay in ms to apply before running the utility
+    #[structopt(short = "d")]
+    delay: Option<u64>,
+    /// The kill signal to use, defaults to SIGTERM
+    #[structopt(short = "k", default_value = "SIGTERM")]
+    kill_signal: String,
     /// The utility to run when files change
     utility: Vec<String>,
+
+    /// The currently running utility process
+    #[structopt(skip)]
+    utility_process: Option<Child>,
+    /// Flag to track if is first execution
+    #[structopt(skip)]
+    first_run: bool,
 }
 
 impl IoWatch {
@@ -50,6 +66,7 @@ impl IoWatch {
         rx: &Receiver<DebouncedEvent>,
         mut watcher: RecommendedWatcher,
     ) -> Result<(), anyhow::Error> {
+        self.first_run = true;
         self.utility = if !self.use_shell {
             self.utility
         } else {
@@ -125,17 +142,48 @@ impl IoWatch {
         Ok(())
     }
 
+    /// Kill the utility if still running
+    fn kill_utility(&mut self) -> Result<(), anyhow::Error> {
+        // TODO(mauri870): use the more generic approach below for windows
+        // match &mut self.utility_process {
+        //     Some(child) => child.kill().with_context(|| format!("failed to kill child process")),
+        //     None => Ok(()),
+        // }
+        match &mut self.utility_process {
+            Some(child) => signal::kill(Pid::from_raw(child.id() as i32), self.kill_signal.parse::<Signal>()?)
+                .with_context(|| format!("failed to kill child process")),
+            None => Ok(()),
+        }
+    }
+
+    /// Wait for a delay in ms
+    fn wait_delay(&self) -> Result<(), anyhow::Error> {
+        thread::sleep(Duration::from_millis(self.delay.unwrap_or(0)));
+        Ok(())
+    }
+
     /// Run the provided utility
-    fn run_utility(&self) -> Result<(), anyhow::Error> {
+    fn run_utility(&mut self) -> Result<(), anyhow::Error> {
+        if self.utility_process.is_some() {
+            self.kill_utility()?;
+        }
+
         if self.clear_term {
             self.clear_term_screen()
                 .context("Failed to clear terminal screen")?;
         }
 
-        Command::new(&self.utility[0])
+        // apply delay only on subsequent runs
+        if !self.first_run && self.delay.is_some() {
+            self.wait_delay()?
+        }
+
+        self.utility_process = Some(Command::new(&self.utility[0])
             .args(&self.utility[1..])
             .spawn()
-            .with_context(|| format!("failed to run the provided utility: {}", &self.utility[0]))?;
+            .with_context(|| format!("failed to run the provided utility: {}", &self.utility[0]))?);
+
+        self.first_run = false;
 
         Ok(())
     }
